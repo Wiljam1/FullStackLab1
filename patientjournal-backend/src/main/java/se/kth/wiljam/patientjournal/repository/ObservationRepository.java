e3e68422f2748a7d15acc83147d56339d8a37488
Wiljam Wilmi <wiljam.wilmi@hotmail.com>
2023-11-20 14:03:05 +0100

    Cleanup

--- patientjournal-backend/src/main/java/se/kth/wiljam/patientjournal/repository/ObservationRepository.java
+++
@@ -1,8 +0,0 @@
-package se.kth.wiljam.patientjournal.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import se.kth.wiljam.patientjournal.model.Observation;
-import se.kth.wiljam.patientjournal.model.User;
-
-public interface ObservationRepository extends JpaRepository<Observation, Long> {
-}
--- patientjournal-backend/src/main/java/se/kth/wiljam/patientjournal/services/UserService.java
+++ patientjournal-backend/src/main/java/se/kth/wiljam/patientjournal/services/UserService.java
@@ -16,11 +16,9 @@ public class UserService {
     @Autowired
     private UserRepository userRepository;
 
-    //@Transactional
     public User create(User user) {
         return userRepository.save(user);
     }
-
 
     public List<User> getAllUsers() {
         return userRepository.findAll();
@@ -51,28 +49,4 @@ public class UserService {
         return userRepository.findByPatientProfileIsNotNull();
     }
 
-    public List<User> getUsersWithStaffIdNotNull() {
-        return userRepository.findByStaffProfileIsNotNull();
-    }
-
-    /* --- UNUSED METHODS
-    public User edit(User newUser, Long id) {
-        return userRepository.findById(id)
-                .map(user -> {
-                    user.setName(newUser.getName());
-                    user.setUsername(newUser.getUsername());
-                    user.setEmail(newUser.getEmail());
-                    return userRepository.save(user);
-                })
-                .orElseThrow(() -> new UserNotFoundException(id));
-    }
-
-    public String delete(Long id) {
-        if(!userRepository.existsById(id)) {
-            throw new UserNotFoundException(id);
-        }
-        userRepository.deleteById(id);
-        return "User with id " + id + " has been deleted sucessfully!";
-    }
-    */
 }
